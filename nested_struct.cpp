d6eb3afe9437f0fc7540e1527b248afa18e44f10
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-04-20 16:09:51 +0700

    menampilkan data

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -29,4 +29,16 @@ int main()
         cout <<"Masukan provisi = ";
         cin >> mhs[i].alamat.provinsi;
     }
-    
+
+        cout << endl;
+        cout << "Menampilkan Data" << endl;
+        cout << endl;
+        for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Kota = " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi = " << mhs[i].alamat.provinsi << endl;
+    }
+}    
