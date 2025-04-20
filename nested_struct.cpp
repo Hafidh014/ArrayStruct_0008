d44b75c2744c5b903b7f9043019bcb197d405fb8
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-04-20 16:05:03 +0700

    object dalam array

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -13,3 +13,19 @@ struct Mahasiswa
     string nim;
     DetailAlamat;
 };
+int main()
+{
+    // membuat object struct dalam array 
+    Mahasiswa mhs[2];
+    for (int i = 0; i <2; i++)
+    {
+        cout <<"Mahasiswa ke-" << i + 1 <<endl;
+        cout <<"Masukan nim =";
+        cin >>mhs[i].nim;
+        cout <<"Masukan nama = ";
+        cin >> mhs[i].nama;
+        cout <<"Masukan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout <<"Masukan provisi = ";
+        cin >> mhs[i].alamat.provinsi;
+    }
